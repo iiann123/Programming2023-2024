9d82fbfd86b750e1d9e5b5f126ad73d8eb847b66
Ian Weng <IanWeng528@gmail.com>
2023-10-16 11:57:58 -0700

    star war bot!

---
+++ Notes/Star_wars_bot.py
@@ -0,0 +1,21 @@
+#Ian Weng
+# 10/16
+# starwars bot
+
+import time
+
+print("I will decide if you can join the darkside")
+time.sleep(1)
+
+fav_color = input("do you like the color red")
+cape = input("do you like capes?")
+
+if fav_color.lower().strip(",. ?!") == "yes":
+    print("dark side you are")
+elif cape.lower().strip(",. ?!") == "yes":
+    print("dark side you are")
+elif fav_color.lower().strip(",. ?!") == "no":
+    print("light side")
+elif cape.lower().strip(",. ?!") == "no":
+    print("light side")
+else: print("light side")
