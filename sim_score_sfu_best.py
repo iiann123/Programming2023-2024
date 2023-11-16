fdac4040c92c78c63ce368e523dac8cbe01d09b3
Ian Weng <IanWeng528@gmail.com>
2023-11-16 10:57:26 -0800

    k

--- sim_score_sfu_best.py
+++ sim_score_sfu_best.py
@@ -1,34 +1,61 @@
-#SFUs BEst SIMILARITY SCORE
-# AUTHOR IAN
-# 10/11/2023
+# SFUs Best - Similarity Score
+# Author: Ian
+# 10 November 2023
 
 # Load data from a file
-# "Read" it in a meaningful way
-# Link our Sim Score algp to the data
+# "Read" it in a meaningful
+# Link our Sim Score algo to the data
+
 # Open the file
 with open("./data.csv") as f:
-    # Get the first line of data
-    print(f.readline())
-    # Get the second line of data
-    print(f.readline())
-# Create a "profile" of likes of places in SFU
-profile = ["Bubble World",
-           "Chef Huang",
-           "Uncle Fatih's",
-           "Guadalupe (MBC)",
-           "Steve's Poke Bar"]
+    f.readline()
+
+    # The second line of the csv file
+    f.readline()
+
+# Create a "profile" of likes (fave places
+# in SFU)
+profile = [
+    "Bubble World",
+    "Chef Hung",
+    "Uncle Fatih's",
+    "Guadalupe (MBC)",
+    "Steve's Poke Bar"
+]
+
+# Initialize top score and their name
+most_similar_score = 0
+most_similar_name = ""
+
 with open("./data.csv") as f:
-    # Throw away trhe header
+    # Throw away the header
     header = f.readline()
+
     for line in f:
         # Convert the string to a list
         current_likes = line.split(",")
+
         # Store the person's name
         current_name = current_likes[1]
+        
         # Initialize the current sim score
         current_sim_score = 0
+
+        # Sim score algorithm
         for item in profile:
-             if item in current_likes:
-                 current_sim_score += 1
+            if item in current_likes:
+                current_sim_score += 1
+        
         # Print the results from this line of data
         print(f"{current_name} - Score: {current_sim_score}")
+
+        # Update the most similar person
+        if current_sim_score > most_similar_score:
+            most_similar_score = current_sim_score
+            most_similar_name = current_name
+
+print("🏅🏅🏅🏅 Most similar person! 🏅🏅🏅🏅")
+print(f"{most_similar_name} - Score: {most_similar_score}")
+
+
+
