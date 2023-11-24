8dd2352f21f50bd7c2d49fa029c61ec1885664c3
Ian Weng <IanWeng528@gmail.com>
2023-11-24 11:11:53 -0800

    dsa

---
+++ Notes/stars.py
@@ -0,0 +1,6 @@
+def stars(numstars: int) -> str:
+    num = input("give number for number of stars")
+    print(f"* *")
+    return num
+
+stars(2) #**
--- cookies.py
+++ cookies.py
@@ -1,34 +1,62 @@
-
 # Cookies
-# Author: Ian
+# Author: ian
 # 21 November 2023
 
+import random
 import turtle
+
+HEIGHT = turtle.window_height() // 2
+WIDTH = turtle.window_width() // 2
 
 # Make baker turtle
 baker_turtle = turtle.Turtle()
 baker_turtle.color("brown")
+baker_turtle.fillcolor("brown")
+baker_turtle.speed(0)
 
-# Draw outline of cookie
-baker_turtle.penup()
-baker_turtle.goto(-5, -30)
-baker_turtle.pendown()
-baker_turtle.circle(30)
-baker_turtle.penup()
 
-# Add 5 chips
-baker_turtle.color("black")
-# center
-baker_turtle.goto(0,0)
-baker_turtle.stamp()
-# top right
-baker_turtle.goto(0,10)
-baker_turtle.stamp()
+def make_cookie(x: int, y: int):
+    """Draws a cookie on the screen at (x, y)
 
-baker_turtle.goto(10,10)
-baker_turtle.stamp()
+    Params:
+    x - x-coordinate of the centre of cookie
+    y - y-coordinate of the centre of cookie
+    """
+    # Set up the outline of the cookie
+    baker_turtle.color("brown")
+    baker_turtle.penup()
+    baker_turtle.goto(-5 + x, -30 + y)
+    baker_turtle.pendown()
+    baker_turtle.begin_fill()
+    baker_turtle.circle(30)
+    baker_turtle.end_fill()
+    baker_turtle.penup()
 
-baker_turtle.stampgoto(-10,-10)
-baker_turtle.stamp()
+    # Add five chips
+    baker_turtle.color("black")
+
+    # Center
+    baker_turtle.goto(0 + x, 0 + y)
+    baker_turtle.stamp()
+
+    # Top-right chip, bottom-right, bottom-left, top-left
+    baker_turtle.goto(10 + x, 10 + y)
+    baker_turtle.stamp()
+    baker_turtle.goto(10 + x, -10 + y)
+    baker_turtle.stamp()
+    baker_turtle.goto(-10 + x, -10 + y)
+    baker_turtle.stamp()
+    baker_turtle.goto(-10 + x, 10 + y)
+    baker_turtle.stamp()
+
+
+make_cookie(100, 100)
+make_cookie(0, 0)
+
+for _ in range(1000):
+    make_cookie(
+        random.randint(-HEIGHT + 30, HEIGHT - 30),
+        random.randint(-WIDTH + 30, WIDTH - 30),
+    )
 
 turtle.done()
